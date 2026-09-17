01272b78962013c77c204bbf5d735bfb041448ed
Tyler Bernett <bernettc@mail.uc.edu>
2026-09-17 15:07:51 -0400

    Initial commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,10 @@
+public class EvenOdd {
+    static void main() {
+        int valToTest = 8;
+
+        IO.println("If val mod 2 = 0 then it is even");
+        IO.println("If val mod 2 = 1 then it is odd");
+
+        IO.println(valToTest % 2);
+    }
+}
